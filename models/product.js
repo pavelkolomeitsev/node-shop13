ee7bcce92e0de343e7fcf06b567ca2045c534d01
Pavel Kolomeitsev <pavelkolomeitsev1981@gmail.com>
2020-02-19 11:34:27 +0200

    implement update/delete operations

--- models/product.js
+++ models/product.js
@@ -3,20 +3,23 @@ const mongoDB = require('mongodb');
 const getDB = require('../util/database').getDB;
 
 class Product {
-    constructor(title, price, imageUrl, description) {
+    constructor(title, price, imageUrl, description, id) {
         this.title = title;
         this.price = price;
         this.imageUrl = imageUrl;
         this.description = description;
+        this._id = id ? new mongoDB.ObjectId(id) : null;
     }
 
     save() {
         const db = getDB();
-        return db.collection('products').insertOne(this)
-            .then(result => {
-
-            })
-            .catch(error => console.log(error));
+        let dbOptinal;
+        if (this._id) {
+            dbOptinal = db.collection('products').updateOne({ _id: this._id }, { $set: this });
+        } else {
+            dbOptinal = db.collection('products').insertOne(this);
+        }
+        return dbOptinal;
     }
 
     static fetchAll() {
@@ -33,6 +36,11 @@ class Product {
         // _id is a BSON-object and productId is just a string
         // to solve this issue we create new ObjectId instance and pass productId as a param
         return db.collection('products').find({ _id: new mongoDB.ObjectId(productId) }).next();
+    }
+
+    static deleteById(productId) {
+        const db = getDB();
+        return db.collection('products').deleteOne({ _id: new mongoDB.ObjectId(productId) });
     }
 }
 
