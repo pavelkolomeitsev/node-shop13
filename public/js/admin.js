01a299a246c4859ad1a8a9eb07e9c635ba2b5904
Pavel Kolomeitsev <pavelkolomeitsev1981@gmail.com>
2020-03-24 12:11:07 +0200

    add async requests from client

---
+++ public/js/admin.js
@@ -0,0 +1,22 @@
+const deleteProduct = (button) => {
+    const prodId = button.parentNode.querySelector('[name=productId]').value;
+    const csrf = button.parentNode.querySelector('[name=_csrf]').value;
+
+    const productElement = button.closest('article');
+
+    fetch('/admin/product/' + prodId, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token': csrf
+        }
+    })
+        .then(result => {
+            return result.json();
+        })
+        .then(data => {
+            productElement.parentNode.removeChild(productElement);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
